28e9fa413ede778f4e9c1308d35ef2160275e8d9
Ivan <sadikov.04@list.ru>
2026-03-01 19:59:03 +0300

    fix Progress object

--- GeneralValues/Progress.cpp
+++ GeneralValues/Progress.cpp
@@ -5,9 +5,17 @@
 
 namespace GeneralValues
 {
-Progress::Progress(Amount&& currentResult, Amount&& goal):m_currentResult(std::move(currentResult)),
-                                                          m_goal(std::move(goal))
-{}
+Progress::Progress(Amount&& currentResult, Amount&& goal):m_currentResult(std::move(currentResult))
+{
+    checkGoal(goal);
+    m_goal = std::move(goal);
+}
+
+void Progress::checkGoal(Amount goal) const
+{
+    if (goal.value() < s_minGoalValue)
+        throw std::runtime_error("Error goal value");
+}
 
 void Progress::changeCurrentResult(Amount newResult)
 {
@@ -16,11 +24,13 @@ void Progress::changeCurrentResult(Amount newResult)
 
 void Progress::changeGoal(Amount goal)
 {
+    checkGoal(goal);
     m_goal = goal;
 }
 
 double Progress::completed() const
 {
+    checkGoal(m_goal);
     double result {m_currentResult.value() / m_goal.value()};
     if (result > 1.0)
         result = 100.0;
--- GeneralValues/Progress.hpp
+++ GeneralValues/Progress.hpp
@@ -6,8 +6,10 @@ namespace GeneralValues
 {
 class Progress
 {
+    static constexpr double s_minGoalValue {1.0};
     Amount m_currentResult;
     Amount m_goal;
+    void checkGoal(Amount goal) const;
 public:
     Progress() = default;
     explicit Progress(Amount&& currentResult, Amount&& goal);
