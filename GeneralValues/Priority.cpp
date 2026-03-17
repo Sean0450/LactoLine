2f361c203df178b6f2583a3b358d6ac381fda251
Ivan <sadikov.04@list.ru>
2026-03-17 22:32:03 +0300

    feat(LactoLine): create Priority metric

---
+++ GeneralValues/Priority.cpp
@@ -0,0 +1,31 @@
+#include "Priority.hpp"
+#include <stdexcept>
+
+namespace GeneralValues
+{
+void Priority::changeReleaseDate(const Date::Date& newReleaseDate, const Date::Date& currentDate)
+{
+    Date::DateDistance timeToComplete = newReleaseDate.compareDate(currentDate);
+    if (timeToComplete.isDistanceEqualOrMore(s_lowPriorityMarker))
+        m_prioity = PriorityStatus::Low;
+    else if (timeToComplete.isDistanceEqualOrMore(s_mediumPriorityMarker))
+        m_prioity = PriorityStatus::Medium;
+    else if (timeToComplete.isDistanceEqualOrMore(s_highProrityMarker))
+        m_prioity = PriorityStatus::High;
+    else if (timeToComplete.isDistanceEqualOrMore(s_extraPriorityMarker))
+        m_prioity = PriorityStatus::Extra;
+    else
+        throw std::runtime_error("Release date too close");
+}
+
+void Priority::changePriority(PriorityStatus newStatus)
+{
+    m_prioity = newStatus;
+}
+
+PriorityStatus Priority::status() const
+{
+    return m_prioity;
+}
+
+}
---
+++ GeneralValues/Priority.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <utility>
+
+#include "../Date/Date.hpp"
+
+namespace GeneralValues
+{
+enum class PriorityStatus
+{
+    Low =    0, 
+    Medium = 1, 
+    High =   2, 
+    Extra =  3
+};
+
+class Priority
+{
+    Date::Date m_releaseDate;
+    PriorityStatus m_prioity {PriorityStatus::Low};
+    static constexpr Date::DateDistance s_lowPriorityMarker{0, 1, 0};
+    static constexpr Date::DateDistance s_mediumPriorityMarker{0, 0, 14};
+    static constexpr Date::DateDistance s_highProrityMarker {0, 0, 7};
+    static constexpr Date::DateDistance s_extraPriorityMarker {0, 0, 3};
+public:
+    Priority() = default;
+    template <typename ReleaseDate, typename CurrentDate>
+    Priority(ReleaseDate&& releaseDate, CurrentDate&& currentDate): m_releaseDate(std::forward<ReleaseDate>(releaseDate))
+    {
+        changeReleaseDate(m_releaseDate, currentDate);
+    }
+    void changeReleaseDate(const Date::Date& newReleaseDate, const Date::Date& currentDate);
+    void changePriority(PriorityStatus newStatus);
+    PriorityStatus status() const;
+};
+}
