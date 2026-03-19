4b06685d0d2273a838a7d0796448097c06e02a17
Ivan <sadikov.04@list.ru>
2026-03-19 22:06:40 +0300

    LactoLine: improve Date object logic

--- Date/Date.cpp
+++ Date/Date.cpp
@@ -49,7 +49,10 @@ void Date::setDate(std::string_view date)
     m_distance.month = std::stoi(date.substr(3, 2).data());
     m_distance.year = std::stoi(date.substr(6, 4).data());
     if (dayCount(static_cast<MonthName>(m_distance.month), m_distance.year) < m_distance.day)
+    {
+        m_distance = DateDistance();
         throw std::runtime_error("Invalid days count");
+    }
 }
 
 bool Date::isLeapYear(int year) const
@@ -94,27 +97,34 @@ DateDistance Date::compareDate(const Date& date) const
         throw std::runtime_error("Can't compae dates. Current date is empty");
     if (date.m_distance.empty())
         throw std::runtime_error("Can't compare dates. Incomming date is empty");
-    const auto [ day, month, year] = m_distance;
-    const auto [c_day, c_month, c_year] = date.m_distance;
-    int yearsDisnace {year - c_year};
-    if (yearsDisnace > 0)
+
+    int yearsDistance {0};
+    int monthesDistance {0};
+    int daysDistance {0};
+    if (m_distance.isDistanceEqualOrMore(date.m_distance))
     {
-        if (month < c_month || (month == c_month && day < c_day))
-            --yearsDisnace;
+        const auto [ day, month, year] = m_distance;
+        const auto [c_day, c_month, c_year] = date.m_distance;
+        yearsDistance = year - c_year;
+        if (yearsDistance > 0)
+        {
+            if (month < c_month || (month == c_month && day < c_day))
+                --yearsDistance;
+        }
+        monthesDistance = month - c_month;
+        if (monthesDistance < 0 || (monthesDistance == 0 && day < c_day))
+        {
+            monthesDistance+= 12;
+        }
+        if (day < c_day && monthesDistance != 0)
+            --monthesDistance;
+        daysDistance = day - c_day;
+        if (daysDistance < 0)
+        {
+            daysDistance = dayCount(static_cast<MonthName>(c_month), c_year) - c_day + day;
+        }
     }
-    int monthesDistance {month - c_month};
-    if (monthesDistance < 0)
-    {
-        monthesDistance+= 12;
-    }
-    if (day < c_day && monthesDistance != 0)
-        --monthesDistance;
-    int daysDistance {day - c_day};
-    if (daysDistance < 0)
-    {
-        daysDistance = dayCount(static_cast<MonthName>(c_month), c_year) - c_day + day;
-    }
-    return {.day=daysDistance, .month=monthesDistance, .year=yearsDisnace};
+    return {.day=daysDistance, .month=monthesDistance, .year=yearsDistance};
 }
 
 std::string Date::getDate() const
