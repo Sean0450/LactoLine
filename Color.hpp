f1dd5e445aeb53e189225aa9283072a234ea4da8
Ivan Sadikov <i.sadikov@yadro.com>
2026-05-16 21:36:56 +0300

    Lactoline: create Color class

---
+++ Color.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <compare>
+
+struct Color
+{
+    int r {0};
+    int g {0};
+    int b {0};
+    int alpha {255};
+
+    constexpr Color() = default;
+    constexpr explicit Color(int nr, int ng, int nb, int nalpha)
+    {
+        if (nr > 0 && nr <= 255) r = nr;
+        if (ng > 0 && ng <= 255) g = ng;
+        if (nb > 0 && nb <= 255) b = nb;
+        if (nalpha > 0 && nalpha <= 255) alpha = nalpha;
+    }
+
+    auto operator<=>(const Color& color) const = default;
+};
+
