2f8b557fbce8b66e4ee652aa4fd3840e8a5245c8
Ivan Sadikov <i.sadikov@yadro.com>
2026-05-18 21:50:04 +0300

    Lactoline: create database class TaskTable

---
+++ Database/TasksTable.hpp
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include <SQLiteCpp/SQLiteCpp.h>
+
+#include "Tasks/TaskData.hpp"
+#include "Tasks/ChangedData.hpp"
+
+class TasksTable
+{
+    std::string m_tableName;
+    SQLite::Database m_database;
+
+    std::string wrapString(const std::string& text);
+
+public:
+    explicit TasksTable(const std::string& databaseName);
+    bool isTableExist() const;
+    bool addTask(const Tasks::TaskData& taskData);
+    void updateTaskData(const ChangedData& data);
+    std::vector<Tasks::TaskData> getTasks() const;
+};
