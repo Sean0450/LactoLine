119acb8bc5c69571b4c732925d5eb42ca26da9b4
Ivan Sadikov <i.sadikov@yadro.com>
2026-05-19 10:23:04 +0300

    Lactoline: add gui object

---
+++ GeneralValues/Gui.cpp
@@ -0,0 +1,21 @@
+﻿#include "Gui.hpp"
+
+#include <random>
+
+namespace GeneralValues
+{
+std::string Gui::generateGui()
+{
+    std::string gui;
+    gui.reserve(s_guiSize);
+    std::mt19937 gen(std::random_device{}());
+    std::uniform_int_distribution<> distr(1000, 9999);
+    for (int i = 0; i < s_sectionsCount; ++i)
+    {
+        gui += std::to_string(distr(gen));
+        if (i != s_sectionsCount - 1)
+            gui += '-';
+    }
+    return gui;
+}
+}
---
+++ GeneralValues/Gui.hpp
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include <string>
+
+namespace GeneralValues
+{
+using GUI = std::string;
+class Gui
+{
+    static constexpr int s_guiSize {19};
+    static constexpr int s_sectionsCount {4};
+public:
+    explicit Gui()=default;
+    static std::string generateGui();
+};
+}
