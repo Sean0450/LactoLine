d4b79946df6e0a7ea5ea647a53901ddf16b89092
Ivan Sadikov <i.sadikov@yadro.com>
2026-05-26 23:12:26 +0300

    Lactoline: create productData DTO

---
+++ Product/ProductData.hpp
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include <string>
+
+struct ProductData
+{
+    std::string productName;
+    std::string categoryName;
+    double unitRawMaterials {0.0};
+    double primeCost        {0.0};
+    double epsilon          {0.0};
+public:
+    explicit ProductData() = default;
+    explicit ProductData(const std::string& productName,
+                         const std::string& categoryName,
+                         double unitRawMaterials,
+                         double primeCost,
+                         double epsilon): productName(productName),
+                                          categoryName(categoryName),
+                                          unitRawMaterials(unitRawMaterials),
+                                          primeCost(primeCost),
+                                          epsilon(epsilon)
+    {
+    }
+};
+
