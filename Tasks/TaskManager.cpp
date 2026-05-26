8662f50059a9436d674298f0addfba04c2ec2aa1
Ivan Sadikov <i.sadikov@yadro.com>
2026-05-26 23:18:53 +0300

    Lactoline: update TaskManager fields and functions realization

--- Tasks/TaskManager.cpp
+++ Tasks/TaskManager.cpp
@@ -1,9 +1,12 @@
 ﻿#include "TaskManager.hpp"
 
+#include <algorithm>
+
 namespace Tasks
 {
-TaskManager::TaskManager(): m_taskTable(s_tableName)
+TaskManager::TaskManager():m_taskTable(s_taskTableName), m_productTable(s_productTableName)
 {
+    readProductFromtable();
     readTasksFromTable();
 }
 
@@ -17,7 +20,7 @@ std::optional<Task> TaskManager::fromDataToTask(const TaskData& data)
         Name taskName {std::move(data.taskName)};
         Amount productAmount{data.productToDoAmount};
         Priority taskPriority {data.status, Date::Date(data.releaseDate)};
-        Product productToCreate {Name("Milk"), Name("M"), Amount(0.5), 5.2, 0.001}; //TODO: заменить реальными значениями
+        Product productToCreate = findProductByName(data.productName);
         Date::Date releaseDate {std::string_view(data.releaseDate)};
         Task task(gui,
                   std::move(taskName),
@@ -52,7 +55,7 @@ TaskData TaskManager::fromTaskToData(const Task& task) const
 
 void TaskManager::readTasksFromTable()
 {
-    auto tasksData = m_taskTable.getTasks();
+    auto tasksData = m_taskTable.getData();
     for (auto& task: tasksData)
     {
         try
@@ -68,13 +71,37 @@ void TaskManager::readTasksFromTable()
     }
 }
 
+void TaskManager::readProductFromtable()
+{
+    auto data = m_productTable.getData();
+    for (const auto& element: data)
+    {
+        std::optional<Product> product = fromDataToProduct(element);
+        if (product)
+        {
+            m_products.emplace_back(std::move(product.value()));
+        }
+    }
+}
+
+Product TaskManager::findProductByName(const std::string& productName)
+{
+    Product result;
+    auto iterator = std::ranges::find_if(m_products, [&](const auto& product){return product.name() == productName;});
+    if (iterator != std::ranges::end(m_products))
+    {
+        result = *iterator;
+    }
+    return result;
+}
+
 void TaskManager::addTask(const TaskData& taskData)
 {
     std::optional<Task> taskCreationResult = fromDataToTask(taskData);
     if (taskCreationResult)
     {
         m_tasks.emplace_back(std::move(taskCreationResult.value()));
-        m_taskTable.addTask(taskData);
+        m_taskTable.addData(taskData);
     }
 }
 
@@ -88,19 +115,62 @@ std::vector<TaskData> TaskManager::getCurrentShiftTaskData() const
     return data;
 }
 
-void TaskManager::taskDataChanged(std::unique_ptr<TaskDataChangedCommand>&& command)
+std::vector<ProductData> TaskManager::getProductData()
 {
-    const GUI gui = command->getTaskData().getIdentifier();
+    std::vector<ProductData> data;
+    for (const auto& product: m_products)
+    {
+        data.emplace_back(product.name(), product.category(), product.unitRowMaterials(), product.primeCost(), product.epsilon());
+    }
+    return data;
+}
+
+std::optional<Product> TaskManager::fromDataToProduct(const ProductData& data)
+{
+    std::optional<Product> result;
+    try
+    {
+        Name productName(data.productName);
+        Name categoryName(data.categoryName);
+        Amount unitRawMaterials(data.unitRawMaterials);
+        result = Product(std::move(productName), std::move(categoryName), std::move(unitRawMaterials), data.primeCost, data.epsilon);
+    }
+    catch(std::exception& exc)
+    {
+        result = std::nullopt;
+    }
+    return result;
+}
+
+void TaskManager::createProduct(const ProductData& data)
+{
+    bool isNameCorrect = std::ranges::none_of(m_products, [&](const auto& product){return product.name() == data.productName;});
+    std::optional<Product> product = fromDataToProduct(data);
+    if (isNameCorrect && product)
+    {
+        m_productTable.addData(data);
+        m_products.emplace_back(std::move(product.value()));
+    }
+}
+
+void TaskManager::taskDataChanged(std::unique_ptr<DataChangedCommand<Tasks::TaskData, Tasks::Task>>&& command)
+{
+    const GUI gui = command->getIdentifier();
     auto iterator = std::ranges::find_if(m_tasks, [gui](const auto& task){return task.gui() == gui;});
     if (iterator != std::ranges::end(m_tasks))
     {
-        Task& task = *iterator;
-        const TaskData data = command->getTaskData();
-        task.changeTaskName(Name(data.taskName));
-        task.changeTaskPriority(data.status);
-        task.changeProductToDoAmount(Amount(data.productToDoAmount));
-        //task.addData(Amount(data.doneProduct)); // Тогда меняется количество, причем неправильным образом, надо чет подумать
-        m_taskTable.updateTaskData(std::move(command));
+        *iterator = command->getUpdatedTask(*iterator);
+        m_taskTable.updateData(std::move(command));
     }
+}
+
+std::vector<std::string> TaskManager::productNames()
+{
+    std::vector<std::string> names;
+    for (const auto& product: m_products)
+    {
+        names.emplace_back(product.name());
+    }
+    return names;
 }
 }
--- Tasks/TaskManager.hpp
+++ Tasks/TaskManager.hpp
@@ -5,25 +5,38 @@
 
 #include "Task.hpp"
 #include "Database/TasksTable.hpp"
-#include "Commands/TaskDataChangedCommand.hpp"
+#include "Database/ProductTable.hpp"
+#include "Commands/DataChangedCommand.hpp"
 #include "TaskChangedObserver.hpp"
+#include "Product/ProductData.hpp"
+#include "Product/ProductChangedObserver.hpp"
 
 namespace Tasks
 {
-class TaskManager: public TaskChangedObserver
+class TaskManager: public TaskChangedObserver,
+                   public ProductChangedObserver
 {
-    static constexpr auto* s_tableName {"Tasks.db3"};
+    static constexpr auto* s_taskTableName {"Tasks.db3"};
+    static constexpr auto* s_productTableName {"Product.db3"};
 
     std::vector<Task> m_tasks;
+    std::vector<Product> m_products;
     TasksTable m_taskTable;
+    ProductTable m_productTable;
 
     std::optional<Task> fromDataToTask(const TaskData& data);
     TaskData fromTaskToData(const Task& task) const;
+    std::optional<Product> fromDataToProduct(const ProductData& data);
     void readTasksFromTable();
+    void readProductFromtable();
+    Product findProductByName(const std::string& productName);
 public:
     explicit TaskManager();
     void addTask(const TaskData& taskData);
     std::vector<TaskData> getCurrentShiftTaskData() const;
-    void taskDataChanged(std::unique_ptr<TaskDataChangedCommand>&& command) override;
+    std::vector<ProductData> getProductData() override;
+    void createProduct(const ProductData& data) override;
+    void taskDataChanged(std::unique_ptr<DataChangedCommand<Tasks::TaskData, Tasks::Task>>&& command) override;
+    std::vector<std::string> productNames() override;
 };
 }
