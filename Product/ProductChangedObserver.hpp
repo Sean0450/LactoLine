587897dfaaf6814640ac82d88d17c0f23380a226
Ivan Sadikov <i.sadikov@yadro.com>
2026-05-26 23:20:53 +0300

    Lactoline: create observers

---
+++ Product/ProductChangedObserver.hpp
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include <vector>
+
+#include "Product/ProductData.hpp"
+
+struct ProductChangedObserver
+{
+    virtual std::vector<ProductData> getProductData() = 0;
+    virtual void createProduct(const ProductData& data) = 0;
+};
---
+++ Tasks/TaskChangedObserver.hpp
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include <vector>
+#include <memory>
+
+#include "Commands/DataChangedCommand.hpp"
+#include "Tasks/TaskData.hpp"
+#include "Tasks/Task.hpp"
+
+struct TaskChangedObserver
+{
+ virtual void taskDataChanged(std::unique_ptr<DataChangedCommand<Tasks::TaskData, Tasks::Task>>&& command) = 0;
+ virtual std::vector<std::string> productNames() = 0;
+};
