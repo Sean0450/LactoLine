f187bcc341b2c9f7bc4d1cfdd8be806ce96e007a
Ivan Sadikov <i.sadikov@yadro.com>
2026-05-26 23:11:37 +0300

    Lactoline: create DateTranslator class

---
+++ Date/DateTranslator.cpp
@@ -0,0 +1,39 @@
+﻿#include "DateTranslator.hpp"
+
+#include "Date.hpp"
+
+GeneralValues::PriorityStatus DateTranslator::calculatePriority(const std::string& viewDateFormat, const std::string& releaseDate)
+{
+    using namespace GeneralValues;
+    return Priority(Date::Date{releaseDate}, Date::Date{viewDateFormat}).status();
+}
+
+bool DateTranslator::isReleaseDateCorrect(const std::string& currentDate, const std::string& releaseDate)
+{
+    const Date::Date current {currentDate};
+    const Date::Date release {releaseDate};
+    return release.isDateMore(current);
+}
+
+std::string DateTranslator::fromChronoDateFormat(const std::chrono::year_month_day& date)
+{
+    std::string correctDate;
+    constexpr int dateLength {10};
+    constexpr auto spliter {'.'};
+    correctDate.reserve(dateLength);
+    unsigned int day = static_cast<unsigned int>(date.day());
+    unsigned int month = static_cast<unsigned int>(date.month());
+    int year = static_cast<int>(date.year());
+    if (day < 10)
+        correctDate += "0" + std::to_string(day);
+    else
+        correctDate += std::to_string(day);
+    correctDate += spliter;
+    if (month < 10)
+        correctDate += "0" + std::to_string(month);
+    else
+        correctDate += std::to_string(month);
+    correctDate += spliter;
+    correctDate += std::to_string(year);
+    return correctDate;
+}
---
+++ Date/DateTranslator.hpp
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include <chrono>
+
+#include "GeneralValues/Priority.hpp"
+
+struct DateTranslator
+{
+    static GeneralValues::PriorityStatus calculatePriority(const std::string& viewDateFormat, const std::string& releaseDate);
+    static bool isReleaseDateCorrect(const std::string& currentDate, const std::string& releaseDate);
+    static std::string fromChronoDateFormat(const std::chrono::year_month_day& date);
+};
