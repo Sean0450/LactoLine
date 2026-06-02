9f6afd193bf49d1775c5bace5f9cfcc0bc70e95c
Ivan Sadikov <i.sadikov@yadro.com>
2026-06-02 18:29:46 +0300

    Lactoline: make taskDataDialog heir of DataCreationDialog

--- Dialogs/TaskDataDialog.cpp
+++ Dialogs/TaskDataDialog.cpp
@@ -1,75 +1,35 @@
-#include "Dialogs/TaskDataDialog.hpp"
-#include "Resources.hpp"
+﻿#include "Dialogs/TaskDataDialog.hpp"
 
 #include <QLabel>
 #include <QHBoxLayout>
 #include <QLineEdit>
 #include <QPushButton>
 
-TaskDataDialog::TaskDataDialog(double createdProduct, QWidget* parent):QDialog{parent},
+TaskDataDialog::TaskDataDialog(double createdProduct, QWidget* parent):DataCreationDialog{parent},
                                                                        m_createdProduct(createdProduct)
 {
-    setModal(true);
-    setWindowTitle("Lactoline");
-    auto* mainLayout = new QHBoxLayout(this);
-    mainLayout->setSpacing(5);
+    setWindowTitle(QStringLiteral("Lactoline"));
 
-    m_description = new QLabel(this);
-    m_description->setFont(m_baseFont);
-    m_description->setText("Введите количество произведенной продукции:");
-    mainLayout->addWidget(m_description);
+    auto* amountEnter = createLineEdit();
+    amountEnter->setText(QString::number(createdProduct));
+    createLineEditLayout(amountEnter, QStringLiteral("Введите количество произведенной продукции:"), [&](){ bool flag {true};
+                                                                                                            double result = m_lineEdits[s_amountIndex]->text().toDouble(&flag);
+                                                                                                            return flag && result > m_createdProduct;});
 
-    m_enterField = new QLineEdit(this);
-    m_enterField->setFont(m_baseFont);
-    m_enterField->setText(QString::number(createdProduct));
-    connect(m_enterField, &QLineEdit::textChanged, this, [&](const QString& text){onProductAmountChanged(text);});
-    mainLayout->addWidget(m_enterField);
-
-    m_enterButton = new QPushButton(this);
-    m_enterButton->setFont(m_baseFont);
-    m_enterButton->setText("Сохранить");
-    m_enterButton->setStyleSheet("QPushButton {" + QString::fromUtf8(Resources::Styles::baseElementsStyle) + "}" +
-                                 QString::fromUtf8(Resources::Styles::pressedButtonStyle));
-    connect(m_enterButton, &QPushButton::clicked, this, &TaskDataDialog::onEnterButtonClicked);
-    mainLayout->addWidget(m_enterButton);
+    auto* rawMaterialsEnter = createLineEdit();
+    createLineEditLayout(rawMaterialsEnter, QStringLiteral("Введите количество затраченного сырья:"), [&](){bool flag {true};
+                                                                                                           m_lineEdits[s_rawMaterialsIndex]->text().toDouble(&flag);
+                                                                                                           return flag;});
+    createButton(QStringLiteral("Добавить"), [&](){m_createdProduct = m_lineEdits[s_amountIndex]->text().toDouble();});
 }
 
-
-void TaskDataDialog::lockDataSending()
-{
-    m_enterButton->setEnabled(false);
-    m_enterField->setStyleSheet("QLineEdit { border: 2px solid red; }");
-}
-
-void TaskDataDialog::onProductAmountChanged(const QString& text)
-{
-    bool flag {true};
-    double result = text.toDouble(&flag);
-    if (flag)
-    {
-        if (result < m_createdProduct)
-        {
-            lockDataSending();
-        }
-        else
-        {
-            m_enterButton->setEnabled(true);
-            m_enterField->setStyleSheet("");
-        }
-    }
-    else
-    {
-        lockDataSending();
-    }
-}
-
-void TaskDataDialog::onEnterButtonClicked()
-{
-    m_createdProduct = m_enterField->text().toDouble();
-    close();
-}
 
 double TaskDataDialog::getCreatedProduct() const
 {
     return m_createdProduct;
+}
+
+double TaskDataDialog::getWastedRawMaterials() const
+{
+    return m_wastedRawMaterials;
 }
--- Dialogs/TaskDataDialog.hpp
+++ Dialogs/TaskDataDialog.hpp
@@ -1,30 +1,18 @@
-#pragma once
+﻿#pragma once
 
-#include <QDialog>
+#include "DataCreationDialog.hpp"
 
-#include "Resources.hpp"
 
-QT_FORWARD_DECLARE_CLASS(QLabel)
-QT_FORWARD_DECLARE_CLASS(QPushButton)
-QT_FORWARD_DECLARE_CLASS(QLineEdit)
-
-class TaskDataDialog: public QDialog
+class TaskDataDialog: public DataCreationDialog
 {
     Q_OBJECT
-
-    const QFont m_baseFont {Resources::baseFont, 12};
+    static constexpr int s_amountIndex {0};
+    static constexpr int s_rawMaterialsIndex {1};
     double m_createdProduct {0.0};
+    double m_wastedRawMaterials {0.0};
 
-    QLabel* m_description {nullptr};
-    QPushButton* m_enterButton {nullptr};
-    QLineEdit* m_enterField {nullptr};
-
-    void lockDataSending();
-
-private slots:
-    void onProductAmountChanged(const QString& text);
-    void onEnterButtonClicked();
 public:
     explicit TaskDataDialog(double createdProduct, QWidget* parent = nullptr);
     double getCreatedProduct() const;
+    double getWastedRawMaterials() const;
 };
