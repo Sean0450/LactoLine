63d9fa9bb90fd2195927166852b2c7003406bbc8
Ivan Sadikov <i.sadikov@yadro.com>
2026-06-02 18:23:31 +0300

    Lactoline: change Resources constants

--- Resources.hpp
+++ Resources.hpp
@@ -32,11 +32,47 @@ static constexpr auto* redBorderLineEdit {"QLineEdit {"
                                           "border-radius: 5px;"
                                           "text-align: center;"
                                           "background-color: white;}"};
+static constexpr auto* baseLineEdit {"QLineEdit {"
+                                     "border: 2px solid grey;"
+                                     "border-radius: 5px;"
+                                     "text-align: center;"
+                                     "background-color: white;}"};
+static constexpr auto* plusButton {"QPushButton{ border: 2px solid grey;"
+                                   "border-radius: 5px;"
+                                   "image: url(:/images/images/Plus.png);"
+                                   "width: 17px;"
+                                   "height: 17px;"
+                                   "text-align: center;"
+                                   "background-color: white;}"
+                                   "QPushButton:pressed {"
+                                   "border: 5px solid grey;"
+                                   "image: url(:/images/images/Plus.png);"
+                                   "width: 17px;"
+                                   "height: 17px;"
+                                   "border-radius: 5px;"
+                                   "text-align: center;"
+                                   "background-color: grey;}"};
+static constexpr auto* deleteButton {"QPushButton{ border: 2px solid grey;"
+                                     "border-radius: 5px;"
+                                     "image: url(:/images/images/Delete.png);"
+                                     "width: 17px;"
+                                     "height: 17px;"
+                                     "text-align: center;"
+                                     "background-color: white;}"
+                                     "QPushButton:pressed {"
+                                     "border: 5px solid grey;"
+                                     "image: url(:/images/images/Delete.png);"
+                                     "width: 17px;"
+                                     "height: 17px;"
+                                     "border-radius: 5px;"
+                                     "text-align: center;"
+                                     "background-color: grey;}"};
 }
 static constexpr auto* baseFont {"Roboto"};
 static constexpr auto* dateParseFormat {"dd.MM.yyyy"};
-static constexpr Color extraPriority {252, 61, 3, 255};
-static constexpr Color highPriority {255, 140, 0, 255};
-static constexpr Color mediumPriority {30, 144, 255, 255};
-static constexpr Color lowPriority {0, 250, 154, 255};
+static constexpr Color birghtRed {252, 61, 3, 255};
+static constexpr Color brightOrange {255, 140, 0, 255};
+static constexpr Color deepBlue {30, 144, 255, 255};
+static constexpr Color brightGreen {0, 250, 154, 255};
+static constexpr Color classicGreen {0, 128, 0, 255};
 }
